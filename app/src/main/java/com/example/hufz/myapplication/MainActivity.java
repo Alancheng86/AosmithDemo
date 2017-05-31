c6cce9998afcd7a1041b12b4a3be3cb4c433c40f
hufz <13773131211@qq.com>
2017-05-31 17:28:07 +0800

    修改视频文件地址为“path="/mnt/extsd0/1.mp4";推测不同系统对SD卡路径认定不同。 该版本可在小黑板Android4.2上运行（视频文件放在外置SD卡中）

--- app/src/main/java/com/example/hufz/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hufz/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hufz.myapplication;
 
 import java.io.File;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;

9da538e6a03d3a28bb7476382e23bb7de118953a
tkgg <371403578@qq.com>
2017-06-02 10:51:00 +0800

    新工程首次导入

--- app/src/main/java/com/example/hufz/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hufz/myapplication/MainActivity.java
@@ -1,134 +1,65 @@
 package com.example.hufz.myapplication;
-
-import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ListView;
-import android.app.ActionBar;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TempControlView tempControl;
-    private String[] areas = new String[]{"1H","2H", "3H", "4H", "5H", "6H", "7H","8H", "9H", "10H", "11H", "12H", "13H","14H", "15H", "16H", "17H", "18H", "19H","20H", "21H", "22H", "23H" , "24H"};
-    private boolean[] areaState=new boolean[]{false, false, false, false, false, true,true, true, false, false, false, true,true, false, false, false, true, true,true, true, true, false, false, false };
-    private ListView areaCheckListView;
-    private Button Button;
-    private View mContentView;
-    private Button button;
-    private Button Button_video;
-    private boolean bcf=true;
-    ActionBar actionBar;
-
+public class MainActivity extends Activity {
+    // 要切换的照片，放在drawable文件夹下
+    int[] images = { R.drawable.a2, R.drawable.a3, R.drawable.a4, };
+    // Message传递标志
+    int SIGN = 16;
+    // 照片索引
+    int num = 0;
+    View mview;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //去除title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         //去掉Activity上面的状态栏
         getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,
                 WindowManager.LayoutParams. FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
-
-        button=(Button)findViewById(R.id.button);
-        mContentView = findViewById(R.id.fullscreen_content_controls);
-        tempControl = (TempControlView) findViewById(R.id.temp_control);
-
-        tempControl.setTemp(25, 85, 25);
-        tempControl.setOnTempChangeListener(new TempControlView.OnTempChangeListener() {
+        final ImageView image = (ImageView) findViewById(R.id.image);
+        mview=findViewById(R.id.mview);
+        final Handler handler = new Handler() {
             @Override
-            public void change(int temp) {
-              //  Toast.makeText(MainActivity.this, temp + "°", Toast.LENGTH_SHORT).show();
+            public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+                super.handleMessage(msg);
+                if (msg.what == SIGN) {
+                    image.setImageResource(images[num++]);
+                    if (num >= images.length) {
+                        num = 0;
+                        mview.setBackgroundColor(0xffff00ff);
+                    }
+                    if(num==1){
+                        mview.setBackgroundColor(0xff00ff00);
+                    }
+                    if(num==2){
+                        mview.setBackgroundColor(0xff0000ff);
+                    }
+
+                }
             }
-        });
-        //super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        Button=(Button)findViewById(R.id.Button);
-        Button_video=(Button)findViewById(R.id.button_video) ;
-       // actionBar=getActionBar();
-        //actionBar.hide();
-        Button_video.setOnClickListener(new but_videoplay());
-        Button.setOnClickListener(new CheckBoxClickListener());
-
-        //
+        };
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+            // TODO Auto-generated method stub
+                Message msg = new Message();
+                msg.what = SIGN;
+                handler.sendMessage(msg);
+            }
+        }, 2000, 1500);
     }
-    class but_videoplay implements OnClickListener{
-        public void onClick (View v){
-            Intent intent = null;
-            intent=new Intent(MainActivity.this, VideoViewActivity.class);
-            startActivity(intent);
-        }
-    }
-    //@Override
-    public void onClick(View v) {
-        Intent intent = null;
-        switch (v.getId()) {
-            case R.id.button_video:
-                intent=new Intent(MainActivity.this, VideoViewActivity.class);
-                startActivity(intent);
-                break;
-		/*case R.id.btn_controller:
-			intent=new Intent(MainActivity.this, ControllerActivity.class);
-			startActivity(intent);
-			break;*/
-            default:
-                break;
-        }
-
-    }
-    public void SetBc (View vt){
-       if(bcf) {mContentView.setBackgroundColor(0xff000000);
-           bcf=!bcf;
-       }
-       else {mContentView.setBackgroundColor(0xECF4F9);
-           bcf=!bcf;
-       }
-    }
-
-
-    class CheckBoxClickListener implements OnClickListener{
-        @Override
-        public void onClick(View v) {
-            AlertDialog ad = new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("运行时间")
-                    .setMultiChoiceItems(areas,areaState,new DialogInterface.OnMultiChoiceClickListener(){
-                        public void onClick(DialogInterface dialog,int whichButton, boolean isChecked){
-                            //点击某个区域
-                        }
-                    }).setPositiveButton("确定",new DialogInterface.OnClickListener(){
-                        public void onClick(DialogInterface dialog,int whichButton){
-                            String s = "您选择了:";
-                            for (int i = 0; i < areas.length; i++){
-                                if (areaCheckListView.getCheckedItemPositions().get(i)){
-                                    s += i + ":"+ areaCheckListView.getAdapter().getItem(i)+ "  ";
-                                }else{
-                                    areaCheckListView.getCheckedItemPositions().get(i,false);
-                                }
-                            }
-                            if (areaCheckListView.getCheckedItemPositions().size() > 0){
-                                //Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
-                            }else{
-                                //没有选择
-                            }
-                            dialog.dismiss();
-                        }
-                    }).setNegativeButton("取消", null).create();
-            areaCheckListView = ad.getListView();
-            ad.show();
-        }
-    }
-
-
-
 }
-

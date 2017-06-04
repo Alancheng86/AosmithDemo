a2b851efbb4f524864e9b23824687f125ccdfb68
tkgg <371403578@qq.com>
2017-06-04 20:33:53 +0800

    与test的APP一致，带自启动。

--- app/src/main/java/com/example/hufz/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hufz/myapplication/MainActivity.java
@@ -14,15 +14,13 @@ import android.widget.ImageView;
 
 public class MainActivity extends Activity {
     // 要切换的照片，放在drawable文件夹下
+//    int[] images = { R.drawable.a2, R.drawable.a3, R.drawable.a4, };
     int[] images = { R.drawable.a001,R.drawable.a002, R.drawable.a003, R.drawable.a004, R.drawable.a005,R.drawable.a006,R.drawable.a007,R.drawable.a008, R.drawable.a009,  };
+
     // Message传递标志
     int SIGN = 16;
     // 照片索引
     int num = 0;
-
-    int Time_set1 = 2000;
-    int Time_set2 = 1500;
-
     View mview;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,69 +36,32 @@ public class MainActivity extends Activity {
         final Handler handler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
-                // TODO Auto-generated method stub
+            // TODO Auto-generated method stub
                 super.handleMessage(msg);
                 if (msg.what == SIGN) {
                     image.setImageResource(images[num++]);
-
                     if (num >= images.length) {
                         num = 0;
-                        Time_set1 = 2000;
-                        Time_set2 = 1500;
-                        mview.setBackgroundColor(0xffFFFFff);
-                        //   mview.setBackgroundColor(0xffff00ff);
+                        mview.setBackgroundColor(0xffff00ff);
                     }
-//                    num++;
-//                    if(num==1){//////R
-//                        mview.setBackgroundColor(0xffff0000);
-//                    }
-//                    if(num==2){//////G
+//                    if(num==1){
 //                        mview.setBackgroundColor(0xff00ff00);
 //                    }
-//                    if(num==3){//////B
+//                    if(num==2){
 //                        mview.setBackgroundColor(0xff0000ff);
-//                    }
-//                    if(num==4){//////WHITE
-//                        mview.setBackgroundColor(0xffFFFFff);
-//                    }
-//                    if(num==5){//////BLACK
-//                        mview.setBackgroundColor(0xff000000);
-//                    }
-//                    if(num==6){//////GRAY
-//                        mview.setBackgroundColor(0xff7f7f7f);
-//                    }
-                    if(num>=7){//////WHITE
-                        Time_set1 = 8000;////////延时看似无效果？？？参数无效，直接改回了数值输入。。
-                        Time_set2 = 5000;
-
-//                        image.setVisibility(View.VISIBLE);
-////                        image.setImageResource(images[num]);
-//                        mview.setBackgroundColor(0xffff00ff);
-                    }
-//                    if(num==8){//////WHITE
-//                        image.setImageResource(images[num]);
-//                        mview.setBackgroundColor(0xff00ffff);
-//                        image.setVisibility(View.INVISIBLE);;
-//                    }
-//                    if(num==9){//////WHITE
-//                        image.setImageResource(images[num]);
-//                        mview.setBackgroundColor(0xff00ffff);
-//
-//                       // mview.setBackgroundColor(0xffFFFFff);
 //                    }
 
                 }
             }
         };
-
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                // TODO Auto-generated method stub
+            // TODO Auto-generated method stub
                 Message msg = new Message();
                 msg.what = SIGN;
                 handler.sendMessage(msg);
             }
-        }, 3000, 3000);
+        }, 2000, 1500);
     }
 }
